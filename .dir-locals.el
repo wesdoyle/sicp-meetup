9bc1dbcde971d15f6a3009441e632cd2534df573
theschmocker <jacobcs1996@gmail.com>
2022-10-20 20:24:28 -0500

    wip notes for 1.3.2

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((org-link-file-path-type . 'relative)))
+ (scheme-mode . ((geiser-default-implementation . 'guile)))
+ )
