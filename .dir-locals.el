dfc6c1012fe92a39662888627181af1b5b539247
theschmocker <jacobcs1996@gmail.com>
2022-10-20 21:24:56 -0500

    comment out scheme impl dir-local var

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode . ((org-link-file-path-type . 'relative)))
- (scheme-mode . ((geiser-default-implementation . 'guile)))
+ ;; (scheme-mode . ((geiser-default-implementation . 'guile)))
  )
